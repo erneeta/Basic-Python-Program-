830c0cbb960d09f9e7834cfc91d6bbf159d1b78d
erneeta <146927215+erneeta@users.noreply.github.com>
2025-07-30 11:50:59 +0530

    Add files via upload

---
+++ 1 comment.py
@@ -0,0 +1,10 @@
+print("Hello this is program for comment")
+# hashtag is use for one line comment in python
+
+''' triple quote is use for
+multiline comment in python '''
+
+""" you can use double quote also but always remember
+    you have to use it three times """
+
+
