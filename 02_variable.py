80c6df622c3bac768f12ec783c886fcfad890cc6
erneeta <146927215+erneeta@users.noreply.github.com>
2025-10-30 08:57:17 +0530

    Python variable

---
+++ 02_variable.py
@@ -0,0 +1,33 @@
+# variable is container
+# variable name = value
+a = 2 # integer
+
+b = 3.4 # float
+
+c = "Python" #string always in quote otherwise gives error
+
+d = True # boolean
+d = False
+
+e = None # none type used for nothing
+
+f,g,h = 1,2,3  # declairation of multiple variable
+print(f,g,h)
+
+hello = 28      # variable in variable
+i = hello
+print(i)
+
+# Rules for variable name
+# 1. Consist of alphabets, digit(numbers), underscore
+# 2. Starts with alphabet or underscore _
+# 3. Python keywords are not allowed
+# 4. Whitespace not allowed in between 
+# 5. Case sensitive
+
+
+# When variable consist of more than 1 word then following ways to declare such variable
+myname = "Neeta"  # flat case
+MyName = "Neeta"  # pascle case
+myName = "Neeta"  # camel case
+my_name = "Neeta"  # snake case
