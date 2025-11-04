0730388a39f72be40fc60298fa37e98b5a1b9842
erneeta <146927215+erneeta@users.noreply.github.com>
2025-11-04 09:43:24 +0530

    python data types

---
+++ 03_datatype.py
@@ -0,0 +1,40 @@
+# Python data type
+# type of data store in variable
+
+# Numeric data type
+# 1. Integer
+b = 34
+
+# 2. Float
+c = 5.7
+
+# 3. complex number
+f = 4j
+f = 2+3j  # 2 is real part of number and 3j is imaginary
+f = complex(3,2)
+print(f)
+print(type(f))  # to know  data type of variable
+
+# Sequence datatype
+# 1. Strings
+a = "Python"
+
+# 2. list
+b =[1,2,3,4]
+
+# 3. tuple
+c = (1,2,3,4)
+
+# dictionary
+a = {1:a, 2:b}
+
+# sets
+a ={1,2,3,4,5}
+
+# Boolean
+d = True
+d = False
+
+# None is use for marking nothing
+e = None
+
