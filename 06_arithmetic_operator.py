25d257e7fa5d193b19f559975ba20107835760c9
erneeta <146927215+erneeta@users.noreply.github.com>
2025-11-05 12:32:46 +0530

    python arithmetic operator

---
+++ 06_arithmetic_operator.py
@@ -0,0 +1,17 @@
+# ARITHMETIC OPERATOR
+a = 98
+b = 2
+
+print(a+b) # Addition
+
+print(a-b) # Subtraction
+
+print(a*b) # Multiplication
+
+print(a/b) # Division
+
+print(a**b) # Exponent (power)
+
+print(a%b) # modulus (returns remainder)
+
+print(a//b) # Floor Division ( returns division result but remove part after decimal)
