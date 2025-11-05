86b369ad9418a270c9459753f2285126e75d1b57
erneeta <146927215+erneeta@users.noreply.github.com>
2025-11-05 12:33:57 +0530

    Assignment operator

---
+++ 07_Assignment_operator.py
@@ -0,0 +1,39 @@
+# Assignment operator
+a = 5
+b = 2
+
+c = a + b  # assign values  from right side operand to left operand
+print(c)
+
+a += b
+print(a) # perform arithmetic operation and assign value to left operand
+
+a = 5
+b = 2
+a -= b
+print(a)
+
+a = 5
+b = 2
+a *= b
+print(a)
+
+a = 5
+b = 2
+a /= b
+print(a)
+
+a = 5
+b = 2
+a **= b
+print(a)
+
+a = 5
+b = 2
+a %= b
+print(a)
+
+a = 5
+b = 2
+a //= b
+print(a)
