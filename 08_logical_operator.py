f7c79d64ca759d9f0aa3518d93334c89ee10a6b6
erneeta <146927215+erneeta@users.noreply.github.com>
2025-11-05 12:34:31 +0530

    python logical operator

---
+++ 08_logical_operator.py
@@ -0,0 +1,15 @@
+# logical operator AND
+print("True AND True is: ",True and True)
+print("True AND False is: ",True and False)
+print("False AND True is: ",False and True)
+print("False AND False is: ",False and False)
+
+# logical operator OR
+print("True OR True is: ",True or True)
+print("True OR False is: ",True or False)
+print("False OR True is: ",False or True)
+print("False OR False is: ",False or False)
+
+# logical operator NOT
+print("NOT True is: " ,not True)
+print("NOT False is: " ,not False)
