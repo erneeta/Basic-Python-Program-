c1c50815e7070561588c2edfe807bfc258e00ecd
erneeta <146927215+erneeta@users.noreply.github.com>
2025-11-05 12:33:20 +0530

    python comparision operator

---
+++ 06_Comparision_operator.py
@@ -0,0 +1,16 @@
+# Comparision operator
+
+a = 5
+b = 2
+
+print(a == b)  # equality operator
+
+print(a != b) # not equal
+
+print(a < b)  # less than
+
+print(a <= b) # less than equal to
+
+print(a > b)  #  greater than
+
+print(a >= b) # greater than equal to
