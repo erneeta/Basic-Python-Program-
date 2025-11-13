a78598f8f1602778e66196e54e8311d3cb6e4fa3
erneeta <146927215+erneeta@users.noreply.github.com>
2025-11-13 11:26:31 +0530

    Add files via upload

---
+++ Assignment2rishabh.py
@@ -0,0 +1,14 @@
+# name = input("Enter name of student: ")
+# sub1 = int(input("Enter marks of sub1: "))
+# sub2 = int(input("Enter marks of sub2: "))
+# sub3 = int(input("Enter marks of sub3: ")) 
+# print(f"name of student is {name}")
+# print(f"Percentage is: ",(sub1+sub2+sub3)/3)
+
+user_data ={}
+print(type(user_data))
+user_data['name'] = input("Enter user name: ")
+user_data['age'] = int(input("Enter age of user: "))
+user_data['height'] = float(input('Enter height of user: '))
+user_data['Student'] =input("Are you a student(yes/noneet) ")
+print(user_data)
