02d2aafa5e9fdece9d98975dab181f96cbafe262
erneeta <146927215+erneeta@users.noreply.github.com>
2025-11-13 11:26:05 +0530

    Add files via upload

---
+++ Assignment1rishabh.py
@@ -0,0 +1,13 @@
+# print("Python is fun")
+# a = '"Quotes" and \'SIngle Quotes\' can be tricky'
+# print(a)
+# b ='''Python is fun
+# "Quotes" and 'Single quotes' can be tricky'''
+# print(b)
+
+
+name = "Neeta"
+age = 36
+city = "Pune"
+# following is formating string
+print(f"my name is {name}. my age is {age}. I live in {city}")
